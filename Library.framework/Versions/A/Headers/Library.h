490454c85762eaea642fd2619228c10c9255f726
NguyeNgocAn <an4217832831@gmail.com>
2022-05-04 09:23:24 +0700

    feature
    commit

---
+++ Library.framework/Versions/A/Headers/Library.h
@@ -0,0 +1,18 @@
+//
+//  Library.h
+//  Library
+//
+//  Created by MacMini on 03/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Library : NSObject
+- (double)sum:(double)x and:(double)y;
+
+- (double)mutiplication:(double)x and:(double)y;
+
+- (double)subtraction:(double)x and:(double)y;
+
+- (double)division:(double)x and:(double)y;
+@end
